65ee9e0ad43755756a57ad61af6ff5360ac320de
JOaqko <joaquinayalar13@gmail.com>
2026-04-30 13:54:16 -0400

    Refactor radius calculation and improve output

--- Radius.cpp
+++ Radius.cpp
@@ -5,22 +5,18 @@ using namespace std;
 
 const double PI = 3.1416;
 
-// Function to calculate distance between two points
 double distance(double x1, double y1, double x2, double y2) {
     return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
 }
 
-// Function to calculate radius
 double radius(double x1, double y1, double x2, double y2) {
     return distance(x1, y1, x2, y2);
 }
 
-// Function to calculate circumference
 double circumference(double r) {
     return 2 * PI * r;
 }
 
-// Function to calculate area
 double area(double r) {
     return PI * r * r;
 }
@@ -28,7 +24,6 @@ double area(double r) {
 int main() {
     double x1, y1, x2, y2;
 
-    // Input: center and a point on the circle
     cin >> x1 >> y1;
     cin >> x2 >> y2;
 
@@ -37,7 +32,7 @@ int main() {
     cout << fixed << setprecision(2);
 
     cout << r << endl;
-    cout << 2 * r << endl;                // diameter
+    cout << 2 * r << endl;
     cout << circumference(r) << endl;
     cout << area(r) << endl;
 
